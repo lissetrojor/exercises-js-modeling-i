02fe53678070d93143fced4c2199e02c3191e3fe
Lisset Rojo Ramirez <lrojoramirez@agnesscott.edu>
2020-07-20 11:56:22 -0400

    from Jesse's session

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 1. Fork this repository
 1. Clone the fork to your computer
 1. Follow along!
+
+## Other Things To Model
+1.
--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-console.log('Nothing here yet');
+let person = {
+  firstName: 'Jesse',
+  lastName: 'Farmer',
+  homeTown: 'Elk Rapids',
+  homeState: 'Michigan',
+  currentTown: 'San Francisco',
+  currentState: 'California',
+  height: 70
+};
+
+if (currentGame.score > previousGame.score) {
+  console.log('Good job! You are getting better')
+}
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.height);
+console.log(person.currentState);
+
+console.log('Before:', person.currentTown);
+person.currentTown = 'Palo Alto';
+console.log('After:', person.currentTown);
+
+let someBook = {
+  title: 'Cool Book',
+  author: 'Cool Author',
+  cover: 'Hard Cover',
+  datePublished: 2020,
+  publisher: 'Penguin Randomhouse',
+  genre: 'Non-fiction',
+  subgenre: 'Historical',
+  goodreadsRating: 4.5,
+  readStatus: 'not read',
+  pages: 300,
+  chapters: 10,
+  text: 'Once upon a time...',
+};
+
+console.log(someBook);
+
+let simon = {
+//   buttons: ,
+//   count: ,
+//   //price: 15.95 ,
+//   sounds: ,
+//   start: ,
+//   score: ,
+//   level: ,
+//   typeOfBattery: ,
+//   typeOfThing: 'game',
+//   recommendedAge: 5-99,
+//   //brand: ,
+//   weight: ,
+//   numOfItemsSold: 30325903280932,
+//   datePutOnMarket: 1972,
+};
+
+/*
+A game
+  many Rounds
+    a challenges
+      many Moves
+*/
